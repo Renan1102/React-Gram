2ef7a55401aed1aed861c1e741664aff1cc73918
Renan1102 <renandesouza1102@gmail.com>
2025-05-14 23:06:58 -0300

    ajuste cors novamente

--- frontend/src/utils/config.js
+++ frontend/src/utils/config.js
@@ -1,7 +1,7 @@
 export const api =
   process.env.NODE_ENV === "development"
     ? "http://localhost:5000/api"
-    : "https://react-gram-backend-git-master-renan-de-souzas-projects.vercel.app/api";
+    : "https://react-gram-backend-nine.vercel.app/api";
 export const uploads = "http://localhost:5000/uploads";
 
 export const requestConfig = (method, data, token = null, image) => {
